997ff4296cc1fc000805cc2c1b0b3e09820afdb2
Ben Munro <ben.munro2000@gmail.com>
2023-12-24 14:46:17 -0500

    created base pages and navigation

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLinkWithHref } from '@angular/router';
+import { navbarLinks } from './navbar-items';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [RouterLinkWithHref],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  navbarLinks = navbarLinks;
 }
