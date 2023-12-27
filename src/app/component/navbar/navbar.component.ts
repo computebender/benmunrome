299518304c01d952852685a1a9798d84124e4daa
Ben Munro <ben.munro2000@gmail.com>
2023-12-26 22:01:22 -0500

    added responsive navigation

--- src/app/component/navbar/navbar.component.ts
+++ src/app/component/navbar/navbar.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 import { navbarLinks } from './navbar-items';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { CommonModule } from '@angular/common';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLinkWithHref, RouterLinkActive],
+  imports: [
+    RouterLinkWithHref,
+    RouterLinkActive,
+    FontAwesomeModule,
+    CommonModule,
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   navbarLinks = navbarLinks;
+  faBars = faBars;
+  faTimes = faTimes;
+  mobileNavOpen = false;
+  breakpointObserver = inject(BreakpointObserver);
+
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe(['(max-width: 768px)'])
+      .subscribe((result) => {
+        if (result.matches) {
+          console.log('breaking');
+          this.mobileNavOpen = false;
+        }
+      });
+  }
+
+  toggleMobileNav() {
+    this.mobileNavOpen = !this.mobileNavOpen;
+  }
+
+  hideMobileNav() {
+    this.mobileNavOpen = false;
+  }
 }
