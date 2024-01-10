4e9e5d28614f26025a873124a804a7e0cb412e99
Ben Munro <ben.munro2000@gmail.com>
2024-01-10 15:28:07 -0500

    Add featuredArticles.model.ts and modify ArticleService and BlogStore to support loading and displaying featured articles

---
+++ src/app/model/featuredArticles.model.ts
@@ -0,0 +1,3 @@
+export interface FeaturedArticlesResponse {
+  featuredArticles: string[];
+}
--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -4,6 +4,7 @@ import { ArticleCardGridComponent } from '../../component/article-card-grid/arti
 import { Article } from '../../model/article.model';
 import { ArticleService } from '../../service/article.service';
 import { WelcomeBannerComponent } from '../../component/welcome-banner/welcome-banner.component';
+import { BlogStore } from '../../store/blog.store';
 
 @Component({
   selector: 'app-home',
@@ -17,13 +18,9 @@ import { WelcomeBannerComponent } from '../../component/welcome-banner/welcome-b
   styleUrl: './home.component.scss',
 })
 export class HomeComponent implements OnInit {
-  articles: Article[] = [];
-
-  articleService = inject(ArticleService);
+  readonly blogStore = inject(BlogStore);
 
   ngOnInit() {
-    this.articleService.getArticles().subscribe((articles) => {
-      this.articles = articles;
-    });
+    this.blogStore.loadFeatuedArticleIds();
   }
 }
