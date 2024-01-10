a00a360f09c950c32ca3f196bdae25d2f68e2580
Ben Munro <ben.munro2000@gmail.com>
2024-01-10 17:04:54 -0500

    Refactor article-card component to improve image loading

--- src/app/component/article-card/article-card.component.ts
+++ src/app/component/article-card/article-card.component.ts
@@ -1,11 +1,13 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Router, RouterLinkWithHref } from '@angular/router';
 import { Article } from '../../model/article.model';
+import { Observable, Subject, combineLatest, filter, map, timer } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-article-card',
   standalone: true,
-  imports: [RouterLinkWithHref],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './article-card.component.html',
   styleUrl: './article-card.component.scss',
 })
@@ -14,7 +16,13 @@ export class ArticleCardComponent {
 
   private router = inject(Router);
 
+  imageLoaded = false;
+
   onCardClick() {
     this.router.navigate(['/blog', this.article.id, this.article.slug]);
+  }
+
+  onImageLoad() {
+    this.imageLoaded = true;
   }
 }
