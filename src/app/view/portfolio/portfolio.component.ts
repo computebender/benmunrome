8bc9e069d0c0a631fb1e58de3cc26834d73732e5
Ben Munro <ben.munro2000@gmail.com>
2024-01-11 22:07:10 -0500

    Update portfolio component and add benmunrome.png

--- src/app/view/portfolio/portfolio.component.ts
+++ src/app/view/portfolio/portfolio.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { ImageCardComponent } from '../../component/image-card/image-card.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ImageCardComponent],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrl: './portfolio.component.scss',
 })
-export class PortfolioComponent {
-
-}
+export class PortfolioComponent {}
