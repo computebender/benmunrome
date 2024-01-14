ebbc527c3f919a1624b8b22503227ee91486f082
Ben Munro <ben.munro2000@gmail.com>
2024-01-14 15:42:24 -0500

    Add article component and update routing

--- libs/admin-dashboard/src/lib/admin-dashboard.module.ts
+++ libs/admin-dashboard/src/lib/admin-dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { adminDashboardRoutes } from './admin-dashboard.routes';
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { ArticleComponent } from './view/article/article.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(adminDashboardRoutes)],
-  declarations: [AdminDashboardComponent],
+  declarations: [AdminDashboardComponent, ArticleComponent],
 })
 export class AdminDashboardModule {}
--- libs/admin-dashboard/src/lib/admin-dashboard.routes.ts
+++ libs/admin-dashboard/src/lib/admin-dashboard.routes.ts
@@ -1,6 +1,21 @@
 import { Route } from '@angular/router';
 import { AdminDashboardComponent } from './admin-dashboard.component';
+import { ArticleComponent } from './view/article/article.component';
 
 export const adminDashboardRoutes: Route[] = [
-  { path: '', pathMatch: 'full', component: AdminDashboardComponent },
+  {
+    path: '',
+    component: AdminDashboardComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'article',
+      },
+      {
+        path: 'article',
+        component: ArticleComponent,
+      },
+    ],
+  },
 ];
