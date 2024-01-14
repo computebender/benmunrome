ca3fa7121963c060b20ae5823b24da47d7b2d3d3
Ben Munro <ben.munro2000@gmail.com>
2024-01-14 10:37:55 -0500

    Add Firebase configuration

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { appRoutes } from '../app.routes';
@@ -6,6 +6,10 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { MARKED_OPTIONS, provideMarkdown } from 'ngx-markdown';
 import { markedOptionsFactory } from './markdown.config';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -19,5 +23,20 @@ export const appConfig: ApplicationConfig = {
         useFactory: markedOptionsFactory,
       },
     }),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'benmunrome',
+          appId: '1:69772176473:web:b3222d577c077a39705548',
+          storageBucket: 'benmunrome.appspot.com',
+          apiKey: 'AIzaSyDeZvXjIJfAV-b4oo58xRFliZqBAfjc2NM',
+          authDomain: 'benmunrome.firebaseapp.com',
+          messagingSenderId: '69772176473',
+        }),
+      ),
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
   ],
 };
